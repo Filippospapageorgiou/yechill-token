2133ffd7af3c4bf46a6e1cc0853d3a0f9833d1e3
Filippospapageorgiou <filpap9@gmail.com>
2024-11-24 19:35:12 +0200

    Initial commit:  token website

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-	  adapter: adapter({
-		runtime: 'nodejs20'  // Explicitly specify Node.js 20
-	  })
-	}
-  };
+  kit: {
+    adapter: adapter()
+  }
+};
 
 export default config;
